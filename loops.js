ed68825930880e910cce4ef28f9459126606727d
naidusaladi <79361491+naidusaladi@users.noreply.github.com>
2023-06-11 13:08:25 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,10 @@
+// Given a number, print count of the digits in the number 
+// Given a number, print count of even digits in the number
+// Given a number, print squares of the digits from last digits
+// Given a number, print last digit of a number
+// Given a number, print first digit in the number
+// Given a number, print sum of digits 
+// Given a number, print two smallest digits in the number
+// Given a number, print the digits from left to right 
+// Given a number, print the count of the digits without using loops
+// Given a number, print first and last digits of number 
